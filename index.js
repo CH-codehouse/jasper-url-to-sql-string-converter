b625e95e103a69a3e978b71cd8d492cf912960e3
Can Kurt <can.kurt.aa@gmail.com>
2022-11-16 09:19:39 +0300

    feat: base64 decoder part added

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ const urlStringTextArea = document.getElementById("urlString");
 const sqlStringTextArea = document.getElementById("sqlString");
 const resultTextArea = document.getElementById("result");
 const messageDiv = document.getElementById("message");
-resultTextArea.readOnly = true;
+const decodedUrlStringDiv = document.getElementById("decodedUrlStringDiv");
 urlStringTextArea.oninput = onTextAreaChange;
 sqlStringTextArea.oninput = onTextAreaChange;
+let encParam = null;
 
 function urlToQuery(urlString, sqlString) {
     urlString = urlString.toString();
     sqlString = sqlString.toString();
 
-    const queryString = new URLSearchParams(urlString.split('?')[1]);
+    let [urlBase, queryString] = urlString.split('?');
 
-    for (let pair of queryString.entries()) {
-        field = "\"" + pair[1] + "\""
+    let queryParams = new URLSearchParams(queryString);
+
+    // Decode base64 part
+    encParam = queryParams.get('enc_param');
+    if(encParam != null) {
+        decodedEncParam = atob(encParam);
+        queryStringWithMark = queryString.replace(`enc_param=${encParam}`, `<mark>${decodedEncParam}</mark>`);
+
+        queryString = queryString.replace(`enc_param=${encParam}`, encodeURI(decodedEncParam));
+        queryParams = new URLSearchParams(queryString);
+        
+        const decodedUrlString = urlBase + queryStringWithMark;
+        toggleDecodedUrlStringDiv(decodedUrlString);
+    }
+
+    for (let pair of queryParams.entries()) {
+        field = "'" + pair[1] + "'"
         if (!isNaN(pair[1])) {
             field = pair[1];
         }
@@ -26,6 +42,7 @@ function urlToQuery(urlString, sqlString) {
 
 function onTextAreaChange() {
     resultTextArea.value = urlToQuery(urlStringTextArea.value, sqlStringTextArea.value);
+    toggleDecodedUrlStringDiv();
 }
 
 function copyToClipBoard() {
@@ -38,3 +55,15 @@ function copyToClipBoard() {
     }, 1000);
 }
 
+function toggleDecodedUrlStringDiv(decodedUrlString) {
+    if(encParam != null && decodedUrlString) {
+        decodedUrlStringDiv.innerHTML = `
+            <label>Decoded url string</label><br>
+            <p id="decodedUrlString">${decodedUrlString}</p>
+            <br>
+        `
+    }
+    else if(encParam == null) {
+        decodedUrlStringDiv.innerHTML = "";
+    }
+}
